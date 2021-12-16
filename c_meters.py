3dbc2b149951d94bd3fba49187d019c0ed84768a
Satur <82034427+Satur696@users.noreply.github.com>
2021-12-15 21:17:26 -0300

    Conversão de Metros
    Converte o metro dado do usuário em outras estruturas de medidas. (sem estrutura ++)

---
+++ c_meters.py
@@ -0,0 +1,10 @@
+print("\t\tConversão de Metros")
+print("\t\t     By Satur")
+
+metro = float(input("\nMetro: "))
+km = metro/1000
+print(f"{metro}m em Quilômetros:", km, "\n")
+cent = metro*100
+print(f"{metro}m em Centímetro:", cent, "\n")
+mili = metro*1000
+print(f"{metro}m em Milímetro:", mili)
